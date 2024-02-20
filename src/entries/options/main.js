fd65fd5410f08a234187771546cdf466085d843a
Golan <95919462+Golannnnn@users.noreply.github.com>
2024-02-20 22:08:01 +0200

    Update package version and add support for Android (#5)

--- src/entries/options/main.js
+++ src/entries/options/main.js
@@ -6,13 +6,19 @@ import setStyles from "./setStyles";
 import initMentions from "./mentions";
 
 (async function () {
-  // sets the font and the background image
-  await setStyles();
+  const isAndroid = navigator.userAgent.includes("Android");
+
+  if (!isAndroid) {
+    // sets the font and the background image
+    await setStyles();
+  }
 
   // initializes the mentions table
   initMentions();
 
   // initializes the scroll
-  const scroll = new Scroll();
-  scroll.init();
+  if (!isAndroid) {
+    const scroll = new Scroll();
+    scroll.init();
+  }
 })();
